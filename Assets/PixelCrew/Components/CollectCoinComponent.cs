0b1386a16a79df21401bdfedf548793f0c1bdaf8
Asura <70947709+AsuraDark@users.noreply.github.com>
2025-03-01 00:01:17 +1000

    ДЗ №4

---
+++ Assets/PixelCrew/Components/CollectCoinComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class CollectCoinComponent : MonoBehaviour
+    {
+        private int _countMoney = 0;
+        [SerializeField] private string _tagSilverCoin;
+        [SerializeField] private string _tagGoldCoin;
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag(_tagGoldCoin))
+            {
+                _countMoney += 10;
+                Debug.Log(_countMoney);
+            }
+            else if (other.gameObject.CompareTag(_tagSilverCoin))
+            {
+                _countMoney += 1;
+                Debug.Log(_countMoney);
+            }
+        }
+
+    }
+
+}
--- Assets/PixelCrew/LayerCheck.cs
+++ Assets/PixelCrew/LayerCheck.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LayerCheck : MonoBehaviour
+namespace PixelCrew
 {
-    [SerializeField] private LayerMask _groundLayer;
-    private Collider2D _collider;
-
-    public bool IsTouchingLayer;
-
-    private void Awake()
+    public class LayerCheck : MonoBehaviour
     {
-        _collider = GetComponent<Collider2D>();
-    }
+        [SerializeField] private LayerMask _groundLayer;
+        private Collider2D _collider;
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        IsTouchingLayer = _collider.IsTouchingLayers(_groundLayer);
-    }
+        public bool IsTouchingLayer;
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        IsTouchingLayer = _collider.IsTouchingLayers(_groundLayer);
+        private void Awake()
+        {
+            _collider = GetComponent<Collider2D>();
+        }
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            IsTouchingLayer = _collider.IsTouchingLayers(_groundLayer);
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            IsTouchingLayer = _collider.IsTouchingLayers(_groundLayer);
+        }
     }
 }
+
