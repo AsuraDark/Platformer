14a010575592571fa67daccba8413518cbff2b4c
Asura <70947709+AsuraDark@users.noreply.github.com>
2025-03-05 00:16:49 +1000

    ДЗ №5

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -13,10 +13,17 @@ namespace PixelCrew
 
         private Rigidbody2D _rigidbody;
         private Vector2 _direction;
+        private Animator _animator;
+        private SpriteRenderer _sprite;
+        private static readonly int IsGroundKey = Animator.StringToHash("is-ground");
+        private static readonly int IsRunning = Animator.StringToHash("is-running");
+        private static readonly int VerticalVelociy = Animator.StringToHash("vertical-velocity");
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _sprite = GetComponent<SpriteRenderer>();
         }
 
         public void SetDirection(Vector2 direction)
@@ -29,17 +36,35 @@ namespace PixelCrew
             _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
 
             var isJumping = _direction.y > 0;
+            var isGrounded = IsGrounded();
             if (isJumping)
             {
-                if (IsGrounded())
+                if (isGrounded && _rigidbody.velocity.y <= 0)
                 {
                     _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
                 }
-
             }
+
             else if (_rigidbody.velocity.y > 0)
             {
                 _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+            }
+            _animator.SetBool(IsGroundKey, isGrounded);
+            _animator.SetBool(IsRunning, _direction.x != 0);
+            _animator.SetFloat(VerticalVelociy, _rigidbody.velocity.y);
+
+            UpdateSpriteDirection();
+        }
+
+        private void UpdateSpriteDirection()
+        {
+            if (_direction.x > 0)
+            {
+                _sprite.flipX = false;
+            }
+            else if (_direction.x < 0)
+            {
+                _sprite.flipX = true;
             }
         }
 
